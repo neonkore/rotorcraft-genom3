98ec72ab25f6d98ad000a1fdd0554ef90cd9aba3
Anthony Mallet <anthony.mallet@laas.fr>
2017-05-12 15:22:23 +0200

    Allow a direct connection to a brushless controller (with mkbl sofware)
    While here, require mk{b,f}l >= 1.8 for the set_throttle() service and accept
    connecting to a flymu>=1.8.

--- codels/mikrokopter_comm_codels.c
+++ codels/mikrokopter_comm_codels.c
@@ -306,7 +306,7 @@ mk_connect_start(const char serial[2][64], uint32_t baud,
                  mikrokopter_ids_sensor_time_s *sensor_time,
                  const genom_context self)
 {
-  static const char magic[] = "[?]mkfl1.7*";
+  static const char magic[] = "[?]*[ml][ky][bfm][lu]1.8*";
 
   size_t i;
   int c, s;
