6d19046fc9728213819767642696e17ff0307a6e
Anthony Mallet <anthony.mallet@laas.fr>
2018-07-05 16:34:24 +0200

    Log sensor rate

--- codels/codels.h
+++ codels/codels.h
@@ -36,13 +36,13 @@ struct mikrokopter_log_s {
 
 # define mikrokopter_logfmt	"%g "
 # define mikrokopter_log_header                                         \
-  "ts imu_ts "                                                          \
+  "ts imu_rate motor_rate "                                             \
   "imu_wx imu_wy imu_wz  imu_ax imu_ay imu_az "                         \
   "cmd_v0 cmd_v1 cmd_v2 cmd_v3 cmd_v4 cmd_v5 cmd_v6 cmd_v7 "            \
   "meas_v0 meas_v1 meas_v2 meas_v3 meas_v4 meas_v5 meas_v6 meas_v7 "    \
   "clk0 clk1 clk2 clk3 clk4 clk5 clk6 clk7"
 # define mikrokopter_log_line                                           \
-  "%" PRIu64 ".%09d " "%d.%09d "                                        \
+  "%" PRIu64 ".%09d " mikrokopter_logfmt mikrokopter_logfmt             \
   mikrokopter_logfmt mikrokopter_logfmt mikrokopter_logfmt              \
   mikrokopter_logfmt mikrokopter_logfmt mikrokopter_logfmt              \
                                                                         \
